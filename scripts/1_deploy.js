73d8d45fe65567a69eee3483c1f0ce5ae2d32df0
Peter <pngb4@FLUX-CAPACITOR.localdomain>
2022-08-29 16:46:43 -0700

    2. Smoke Test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,17 @@
+async function main() {
+  // Fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token")
+
+  // Deploy contact
+  const token = await Token.deploy()
+  // Get info that was deployed and load it up to our smart contract
+  await token.deployed()
+  console.log('Token Deployed to: ${token.address}')
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+  console.error(error);
+  process.exitCode = (1);
+});
